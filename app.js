60a612adb4c6864bd027d1cde2588bca0b1b3853
ShilpiLashkari <shilpilashkari@gmail.com>
2023-05-30 17:46:12 +0530

    Render from js file and added css

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 //  Hello World Using React CDN
-const helloHeading = React.createElement("h1", {}, "Hello World from React! ");
+const helloHeading = React.createElement(
+  "h1",
+  { id: "heading", anotherattribute: "userCreatedValue" },
+  "Hello World from React! "
+);
 const reactRoot = ReactDOM.createRoot(document.getElementById("root1"));
 reactRoot.render(helloHeading);
+console.log("Check this out:::", helloHeading);
