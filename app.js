c28f4a65f56fb250b1460fa574cc2c03dcd83de5
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-01 13:33:45 +0530

    Rendering Nested Layout

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ const helloHeading = React.createElement(
 const reactRoot = ReactDOM.createRoot(document.getElementById("root1"));
 reactRoot.render(helloHeading);
 console.log("Check this out:::", helloHeading);
+
+// Creating Nested elements
+
+/** Example:
+ *<div id="parent">
+ *     <div id="child">
+ *          <h1>Hello World from React Nesting</h1>
+ *      </div>
+ *</div>
+ */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hello World from React Nesting")
+  )
+);
+const nestedRoot = ReactDOM.createRoot(document.getElementById("nestedRootId")); // root is html file root id for div
+nestedRoot.render(parent);
