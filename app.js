4da90512d627aba6c18ef69a9f63410992f72426
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-03 09:42:12 +0530

    Clean code and prod-build code

--- app.js
+++ app.js
@@ -1,71 +1,18 @@
-//  Hello World Using React CDN
-const helloHeading = React.createElement(
-  "h1",
-  { id: "heading", anotherattribute: "userCreatedValue" },
-  "Hello World from React! "
-);
-const reactRoot = ReactDOM.createRoot(document.getElementById("root1"));
-reactRoot.render(helloHeading);
-console.log("Check this out:::", helloHeading);
-
-// Creating Nested elements
-
-/** Example:
- *<div id="parent">
- *     <div id="child">
- *          <h1>Hello World from React Nesting</h1>
- *          <h2>Hello From Sibling</h2>
- *      </div>
- *</div>
- */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [
-      React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
-      React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
-    ] // Created array for siblings
-  )
-);
-const nestedRoot = ReactDOM.createRoot(document.getElementById("nestedRootId")); // root is html file root id for div
-nestedRoot.render(parent);
-
-/** Example:
- *<div id="parent">
- *     <div id="child1">
- *          <h1>Hello World from React Nesting</h1>
- *          <h2>Hello From Sibling</h2>
- *      </div>
- *     <div id="child2">
- *          <h1>Hello World from React Nesting</h1>
- *          <h2>Hello From Sibling</h2>
- *      </div>
- *</div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent2 = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child1" },
-    [
-      React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
-      React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
-    ] // Created array for siblings
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    [
-      React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
-      React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
-    ] // Created array for siblings
-  ),
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
+    React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
+    React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
+  ]),
 ]);
 
 const nestedRoot2 = ReactDOM.createRoot(
   document.getElementById("nestedRootId")
-); // root is html file root id for div
+);
 nestedRoot2.render(parent2);
