3ea4488751488e272904fefd3dada369b8684d49
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-04 21:08:24 +0530

    JSX

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1 id="heading">This is the main heading 😎</h1>;
+const jsxHeading = <h1 id="heading">This is the main heading 😎</h1>;
 const root = ReactDOM.createRoot(document.getElementById("nestedRootId"));
-root.render(heading);
+root.render(jsxHeading);
