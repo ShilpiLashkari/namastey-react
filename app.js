53044982ece92dd90b722459d69d0c6c97cdcf8f
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-04 16:48:05 +0530

    JSX intro

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent2 = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
-    React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello World from React Nesting Heading 1"),
-    React.createElement("h2", {}, "Hello World from React Nesting Heading 2"),
-  ]),
-]);
-
-const nestedRoot2 = ReactDOM.createRoot(
-  document.getElementById("nestedRootId")
-);
-nestedRoot2.render(parent2);
+const jsxHeading = <h1 id="heading">This is the main heading 😎</h1>;
+const root = ReactDOM.createRoot(document.getElementById("nestedRootId"));
+root.render(jsxHeading);
