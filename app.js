9acd86d201560d1c6e4f073493e0a316be07cefd
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-05 13:08:52 +0530

    applying class in jsx

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id="heading">This is the main heading 😎</h1>;
+const jsxHeading = (
+  <h1 id="heading" className="heading">
+    This is the main heading 😎
+  </h1>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
