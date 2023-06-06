1a06d44f4b87c86cd9c71bc9bf1b034f5a39574a
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-06 15:26:40 +0530

    Rendering functional component

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 id="heading" className="heading">
-    This is the main heading 😎
-  </h1>
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
-
-const FunctionalComponent = () => {
-  return <h1>Hi There!</h1>;
-};
-const FunctionalComponent2 = () => <h1>Hi There!</h1>;
-
-const FunctionalComponent3 = () => (
+const FunctionalComponent = () => (
   <div id="container">
     <h2 className="heading2">Hi Shilpi</h2>
   </div>
 );
+
+const rootComponent = ReactDOM.root(document.getElementById("container"));
+rootComponent.render(<FunctionalComponent />);
