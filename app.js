b052e98f8a835e08333528f39e97d3248a74c710
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-07 18:44:13 +0530

    React element inside react component

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const reactElement = (
+  <div>
+    <p>Some text inside a react element rendering in MainComponent</p>
+  </div>
+);
 const TitleComponent = () => <h1 className="title">Namastey React </h1>;
 
 const number = 1221;
@@ -12,8 +17,12 @@ const MainComponent = () => (
     <h3>{number}</h3>
     <h4>{120 + 50}</h4>
     <h5>{console.log("Hii")}</h5>
+    <h6>{reactElement}</h6>
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<MainComponent />);
+
+console.log("type 1:", typeof reactElement);
+console.log("type 2:", typeof MainComponent);
