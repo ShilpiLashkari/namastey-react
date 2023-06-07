376d801fb5c3b7569b08560aa4ba2ce16e4b628c
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-07 18:04:40 +0530

    JS expression inside React Functional Component

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = () => {
-  return <h1 className="title">Namastey React </h1>;
-};
+const TitleComponent = () => <h1 className="title">Namastey React </h1>;
 
-const MainComponent = () => {
-  return (
-    <div id="container">
-      <TitleComponent />
-      <h2 className="mainHeading">Hi There! This is heading 2.</h2>
-    </div>
-  );
-};
+const number = 1221;
+
+const MainComponent = () => (
+  <div id="container">
+    <TitleComponent />
+    <h2 className="mainHeading">Hi There! This is heading 2.</h2>
+    <h3>{number}</h3>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<MainComponent />);
