288e9ebf060fbfbe905dda36f292ddea8a45b6ca
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-07 18:15:33 +0530

    JS expression inside the component

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const number = 1221;
 const MainComponent = () => (
   <div id="container">
     <TitleComponent />
-    <h2 className="mainHeading">Hi There! This is heading 2.</h2>
+    <h2 className="mainHeading">Hi Theres! This is heading 2.</h2>
     <h3>{number}</h3>
+    <h4>{120 + 50}</h4>
+    <h5>{console.log("Hii")}</h5>
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
