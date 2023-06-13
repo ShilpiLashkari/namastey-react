55d8e6f9024165804295001f4d920ae5f5e3f1be
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-13 12:30:05 +0530

    Minor changes

--- app.js
+++ app.js
@@ -25,5 +25,6 @@ const MainComponent = () => (
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<MainComponent />);
+
 console.log("type 1:", typeof reactElement);
 console.log("type 2:", typeof MainComponent);
