1a98691593c68989f6c9711d954eb490eb6e2e7b
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-14 18:06:17 +0530

    Multiple root rendering

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const MainComponent = () => (
-  <React.Fragment>
+  <div>
     <div className="container-1">
       <p>This is para 1 using react Fragment</p>
     </div>
     <div className="container-2">
       <p>This is para 2 using react Fragment</p>
     </div>
-  </React.Fragment>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<MainComponent />);
+
+// Another div container
+
+const MainComponent2 = () => (
+  <div>
+    <div className="container-1">
+      <p>This is para 1 using react Fragment</p>
+    </div>
+    <div className="container-2">
+      <p>This is para 2 using react Fragment</p>
+    </div>
+  </div>
+);
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+
+root2.render(<MainComponent2 />);
