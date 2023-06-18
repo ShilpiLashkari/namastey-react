da30fe5bcccdcdc57bda13e36cd934ddcb572c0a
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-18 16:04:52 +0530

    Menu items in component

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const Header = () => {
       <div className="logo-container">
         <img className="logo" src="https://static.vecteezy.com/system/resources/previews/003/513/751/original/cute-monkey-hanging-tree-cartoon-illustration-free-vector.jpg" />
       </div>
-
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
