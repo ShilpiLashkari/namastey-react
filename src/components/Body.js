e1b0862a16c5eefb1b35826b5257743bd2659c67
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-22 21:55:19 +0530

    state variable

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,68 +3,20 @@ import resList from "../utils/MockData";
 import { useState } from "react";
 const Body = () => {
 
-    let listOfRestaurants = [
-        {
-            "data": {
-                "id": "354531",
-                "name": "Burger Seigneur",
-                "cloudinaryImageId": "t8qo8yuimdpduzbozybj",
-                "cuisines": [
-                    "American",
-                    "Continental",
-                    "Beverages"
-                ],
-                "costForTwo": 120000,
-                "deliveryTime": 28,
-                "avgRating": "3.8",
-            },
-        },
-        {
-            "data": {
-                "id": "354532",
-                "name": "Dominos",
-                "cloudinaryImageId": "t8qo8yuimdpduzbozybj",
-                "cuisines": [
-                    "American",
-                    "Continental",
-                    "Beverages"
-                ],
-                "costForTwo": 120000,
-                "deliveryTime": 28,
-                "avgRating": "4.7",
-            },
-        },
-        {
-            "data": {
-                "id": "354533",
-                "name": "MCD",
-                "cloudinaryImageId": "t8qo8yuimdpduzbozybj",
-                "cuisines": [
-                    "American",
-                    "Continental",
-                    "Beverages"
-                ],
-                "costForTwo": 120000,
-                "deliveryTime": 28,
-                "avgRating": "4.1",
-            },
-        }
-    ]
+    let [listOfRestaurants, setListOfRestaurants] = useState(resList)
 
     return (
         <div className="body">
             <div className="search-container">
-                {/* <input type="text" placeholder="Search..." /> */}
-                {/* <button className="search-btn">Search</button> */}
-                <button className="filter-btn" onClick={() => {
-                    listOfRestaurants = listOfRestaurants.filter(res => res.data.avgRating > 4);
-                    console.log(listOfRestaurants);
-                }}>
+                <button className="filter-btn"
+                    onClick={() => {
+                        const filterList = listOfRestaurants.filter((res) => res.data.avgRating > 4);
+                        setListOfRestaurants(filterList);
+                    }}>
                     Top Rated Restaurant</button>
             </div>
             <div className="res-container">
-                {/* {resList.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} />)} */}
-                {listOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} />)}
+                {listOfRestaurants.map((restaurant) => (<RestaurantCard key={restaurant.data.id} resData={restaurant} />))}
             </div>
         </div >
     )
