8e3ded5252446d47a5db0213be31000631e58d27
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-23 19:49:21 +0530

    Code cleaning

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,28 @@
-import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/MockData";
 import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import MockData from "../utils/MockData";
+
 const Body = () => {
+  let [listOfRestaurants, setListOfRestaurants] = useState(MockData);
+  const handleTopRated = () => {
+    const filterList = listOfRestaurants.filter(
+      (res) => res.data.avgRating > 4
+    );
+    setListOfRestaurants(filterList);
+  };
 
-    let [listOfRestaurants, setListOfRestaurants] = useState(resList)
-
-    return (
-        <div className="body">
-            <div className="search-container">
-                <button className="filter-btn"
-                    onClick={() => {
-                        const filterList = listOfRestaurants.filter((res) => res.data.avgRating > 4);
-                        setListOfRestaurants(filterList);
-                    }}>
-                    Top Rated Restaurant</button>
-            </div>
-            <div className="res-container">
-                {listOfRestaurants.map((restaurant) => (<RestaurantCard key={restaurant.data.id} resData={restaurant} />))}
-            </div>
-        </div >
-    )
+  return (
+    <div className="body">
+      <div className="search-container">
+        <button className="filter-btn" onClick={handleTopRated}>
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
 };
-
-export default Body;
