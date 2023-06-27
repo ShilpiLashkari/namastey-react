fc8559fef84e58d14d61ccebc4eb5e666f0bf321
ShilpiLashkari <shilpilashkari@gmail.com>
2023-06-27 22:58:13 +0530

    Using live API

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 const AppLayout = () => {
-  return <div className="app">
-    <Header />
-    <Body />
-  </div >
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/MockData";
 
 const Body = () => {
-  let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  let [listOfRestaurants, setListOfRestaurants] = useState([]);
   const handleTopRated = () => {
     const filterList = listOfRestaurants.filter(
       (res) => res.data.avgRating > 4
     );
     setListOfRestaurants(filterList);
   };
-
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(json.data.cards[2].data.data.cards);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="body">
       <div className="search-container">
@@ -26,3 +34,5 @@ const Body = () => {
     </div>
   );
 };
+
+export default Body;
