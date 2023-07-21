ecf8a88849a10a757a767a570d71f9be13948b4d
ShilpiLashkari <shilpilashkari@gmail.com>
2023-07-21 14:37:22 +0530

    Routing Intro

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
 const AppLayout = () => {
   return (
     <div className="app">
@@ -13,13 +14,13 @@ const AppLayout = () => {
 };
 const appRouter = createBrowserRouter([
   {
-    path: "/",
-    element: <AppLayout />
+    path: "/", //If homepage
+    element: <AppLayout />,
   },
   {
     path: "/about",
-    element: <About />
-  }
-])
+    element: <About />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
