b83096a9d9ce82973a27de5dfead61b4c8979371
ShilpiLashkari <shilpilashkari@gmail.com>
2023-08-30 10:39:40 +0530

    Changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -61,7 +61,7 @@ const Body = () => {
             setFilteredRestaurant(filteredList);
           }}
         >
-          Top Rated Restaurant
+          Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
