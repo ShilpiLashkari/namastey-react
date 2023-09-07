120fec565474a234afbb71833240c6cf2ff6e073
ShilpiLashkari <shilpilashkari@gmail.com>
2023-09-07 16:32:43 +0530

    Changes in functional component

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 const User = ({ nameOfPerson, locationOfPerson, emailOfPerson }) => {
+  const [count] = useState(0);
+  const [count2] = useState(0);
   return (
     <div className="user-card">
       <h2>Name: {nameOfPerson}</h2>
       <h3>Location: {locationOfPerson}</h3>
       <h4>Contact: {emailOfPerson}</h4>
+      <h5>Count: {count}</h5>
+      <h6>Count2:{count2}</h6>
     </div>
   );
 };
