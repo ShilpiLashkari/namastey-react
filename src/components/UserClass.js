509cf17a1e1d59dbde458339461fb80f146258ad
ShilpiLashkari <shilpilashkari@gmail.com>
2023-09-08 17:21:55 +0530

    Changes

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -12,7 +12,7 @@ class UserClass extends React.Component {
   async componentDidMount() {
     const data = await fetch("https://api.github.com/users/ShilpiLashkari");
     const json = await data.json();
-    console.log("data : ", json);
+    //    console.log("data : ", json);
 
     this.setState({
       userInfo: json,
