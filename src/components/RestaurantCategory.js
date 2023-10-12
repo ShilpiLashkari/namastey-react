51f40e696e3767654748e80654d1ba1d2c0672b7
ShilpiLashkari <shilpilashkari@gmail.com>
2023-10-12 16:45:29 +0530

    Lifting the state up

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+
+const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
+  const handleClick = () => {
+    return setShowIndex();
+  };
+  return (
+    <div>
+      {/* Accordion header */}
+      <div
+        className="w-6/12 mx-auto my-6  cursor-pointer bg-gray-50 shadow-lg p-4 "
+        onClick={handleClick}
+      >
+        <div className="flex justify-between">
+          <span className="font-bold text-lg">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span>⬇️</span>
+        </div>
+
+        {showItems && <ItemList items={data.itemCards} />}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCategory;
