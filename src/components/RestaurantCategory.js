748432bc4fe28cab8cd45b97884f5b324219cc40
ShilpiLashkari <shilpilashkari@gmail.com>
2023-10-18 14:36:11 +0530

    Props drilling

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
+const RestaurantCategory = ({ data, showItems, setShowIndex, dummy }) => {
   const handleClick = () => {
     return setShowIndex();
   };
@@ -14,12 +14,12 @@ const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
       >
         <div className="flex justify-between">
           <span className="font-bold text-lg">
-            {data.title} ({data.itemCards.length})
+            {data.title} ({data.itemCards?.length})
           </span>
           <span>⬇️</span>
         </div>
 
-        {showItems && <ItemList items={data.itemCards} />}
+        {showItems && <ItemList items={data.itemCards} dummy="dummy" />}
       </div>
     </div>
   );
