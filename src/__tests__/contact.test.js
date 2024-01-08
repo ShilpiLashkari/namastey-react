eee69c0f26e0d0fd61246cbb34fb2135796b2fe1
ShilpiLashkari <shilpilashkari@gmail.com>
2024-01-08 13:22:59 +0530

    Test cases

--- src/__tests__/contact.test.js
+++ src/__tests__/contact.test.js
@@ -1,8 +1,9 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 
 test("Should load contact us component", () => {
   render(<Contact />);
   const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
+  expect(heading).toBeInTheDocument(); //Assertion
 });
