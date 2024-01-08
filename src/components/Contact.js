a9010d670c40aad1b8ba8e0f10a74a3601e1adb4
ShilpiLashkari <shilpilashkari@gmail.com>
2024-01-08 13:25:13 +0530

    Test cases

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,26 @@
-import React from "react"
+import React from "react";
 
 const Contact = () => {
-    return (
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us page</h1>
+      <form>
+        <input
+          type="text"
+          className="form-control p-2 m-2 border border-black"
+          placeholder="Name"
+        />
         <div>
-            <p>
-                Contact Us page
-            </p>
+          <textarea
+            className="form-control p-2 m-2 border border-black"
+            placeholder="Message"
+          />
         </div>
-    );
-}
+        <button className="p-2 m-2 border border-black bg-black text-white rounded">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 export default Contact;
