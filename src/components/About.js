b3c58df89a0c5b07a60cd80ae1f5eed9ac880418
Shilpi Lashkari <shilpilashkari@gmail.com>
2024-02-04 18:11:50 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <h2>This is namastey react series</h2>
+      <h2>This is namastey react seriess</h2>
       <div>
         <p>Functional Component</p>
         {/* <User
