1d53cc7ff08ea2bc9c6317d7a74249d2b2aefea2
ShilpiLashkari <shilpilashkari@gmail.com>
2024-03-08 16:12:41 +0530

    Integration Testing

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import RestaurantCard, { isOpenOrNOt } from "../RestaurantCard";
+import RestaurantCard from "../RestaurantCard";
 import MOCK_DATA from "../../components/mocks/resCardMock.json";
 import "@testing-library/jest-dom";
 
