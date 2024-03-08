3376e8282c9c2046d839347eb351aaf91eff675a
ShilpiLashkari <shilpilashkari@gmail.com>
2024-03-08 16:12:48 +0530

    Integration Testing

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,7 +1,29 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Body from "../../components/Body";
-
-global.fetch=jest.fn()
-it("Should render the body component with search button", () => {
-  render(<Body />);
+import MOCK_DATA from "../../components/mocks/mockResListData.json";
+import { BrowserRouter } from "react-router-dom";
+import { act } from "react-dom/test-utils";
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+it("Should render the body component with search button", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
+  const btnSearch = screen.getByRole("button", { name: "Search" });
+  expect(btnSearch).toBeInTheDocument();
+  const searchInput = screen.getByTestId("search-input");
+  fireEvent.change(searchInput, { target: { value: "burger" } }); //writing "burger" in input field
+  fireEvent.click(btnSearch); // clicking a button after input
+  const cards = screen.getAllByTestId("resCard");
+  // expect(cards.length).toBe(1);
 });
