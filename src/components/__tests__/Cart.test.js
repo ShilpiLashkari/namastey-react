d6898271d30a0781d43932cbb2afea42f0a89f0c
ShilpiLashkari <shilpilashkari@gmail.com>
2024-04-02 19:21:40 +0530

    Integration testing - last part

--- src/components/__tests__/Cart.test.js
+++ src/components/__tests__/Cart.test.js
@@ -1,13 +1,27 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+
+import MOCK_DATA_NAME from "../mocks/mockResMenu.json";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 import RestaurantMenu from "../RestaurantMenu";
-import MOCK_DATA_NAME from "../../components/mocks/mockResMenu.json";
+import MOCK_DATA_NAME from "../mocks/mockResMenu.json";
 
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve(MOCK_DATA_NAME) })
 );
 it("Should load restaurant menu component", async () => {
-  await act(async () => render(<RestaurantMenu />));
-  const accordionHeader = screen.getByText("Women's Day Specials (11)");
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+          <RestaurantMenu />
+          <Cart />
+        </Provider>
+      </BrowserRouter>
+    )
+  );
+  const accordionHeader = screen.getByText("McSpicy Combos with Sprite (8)");
   fireEvent.click(accordionHeader);
 });
